71475cc22445a253a3b35d7d0fcb541292b919d3
teiron3 <teiron.teiron+git@gmail.com>
2018-02-12 17:44:32 +0900

    クリック操作できるようになった！

---
+++ main_.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class main_class
+{
+    static void Main()
+    {
+    Console.Write("test");
+    System.Threading.Thread.Sleep(1000);
+    test tt = new test{};
+    tt.click_(2000, 400);
+    return;
+    }
+}
